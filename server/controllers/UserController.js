64a354cd2aae611619ab399d0b1e0a321b109df7
kishangupta99 <kishankumar@gmail.com>
2025-05-21 23:48:23 +0530

    Webhooks Added

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,67 @@
-
-// API CONTROLLER FUNCTION TO MANAGE CLERK WITH DATABASE 
-// http://localhost:4000/api/v1/user/webhooks
+import { Webhook } from "svix";
+import userModel from "../models/UserModel.js";
+// API CONTROLLER FUNCTION TO MANAGE CLERK WITH DATABASE
+// http://localhost:4000/api/user/webhooks
 
 const clerkWebhooks = async (req, res) => {
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-}
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
+
+    const { data, tyoe } = req.body;
+
+    switch (type) {
+      case "user.created":
+        const userData = {
+          clerkId: data.id,
+          email: data.email_addresses[0].email_address,
+          firstName: data.first_name,
+          lastName: data.last_name,
+          photo: data.image_url,
+        }
+        await userModel.create(userData);
+        res.json({})
+        break;
+
+      case "user.updated":
+        const updatedUser = {
+          email: data.email_addresses[0].email_address,
+          firstName: data.first_name,
+          lastName: data.last_name,
+          photo: data.image_url,
+        }
+        await userModel.findOneAndUpdate(
+          { clerkId: data.id },
+          updatedUser,
+        );
+        res.json({})
+
+        break;
+
+      case "user.deleted":
+        await userModel.findOneAndDelete
+            ({ clerkId: data.id });
+        res.json({})
+
+        break;
+
+      default:
+        console.log("Unknown event type:", type);
+        break;
+    }
+    return res.status(200).json({ message: "Webhook received successfully" });
+
+
+
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "Invalid signature" });
+  }
+};
+
+export { clerkWebhooks }; 
--- server/server.js
+++ server/server.js
@@ -1,25 +1,24 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import connectDB from './config/mongodb.js';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
-
-// App config 
+// App config
 const PORT = process.env.PORT || 4000;
 const app = express();
-await connectDB ();
+await connectDB();
 
 // initialize middleware
 app.use(express.json());
 app.use(cors());
 
-// API routes 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+// API routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
-
-
