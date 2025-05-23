7a859f476f877d184a0c25075cbdcbe02753433d
kishangupta99 <kishankumar@gmail.com>
2025-05-23 22:50:07 +0530

    new commit

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import { clerkWebhooks } from '../controllers/UserController.js';
+// import express from 'express';
+// import { clerkWebhooks } from '../controllers/UserController.js';
 
 
-const userRouter = express.Router();
+// const userRouter = express.Router();
 
-userRouter.post('/webhooks', clerkWebhooks);
+// userRouter.post('/webhooks', clerkWebhooks);
 
 
-export default userRouter; 
+// export default userRouter; 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/mongodb.js";
-import userRouter from "./routes/userRoutes.js";
+// import userRouter from "./routes/userRoutes.js";
 
 // App config
 const PORT = process.env.PORT || 4000;
@@ -17,7 +17,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("API WORKING");
 });
-app.use("/api/user", userRouter);
+// app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
